f611c29d495e0054c76290912f3e8f3458aa8a7d
victor <thalinhtet000@gmail.com>
2023-04-12 17:04:17 +0630

    Initialized client

---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <router-view/>
+</template>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue,ts}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
